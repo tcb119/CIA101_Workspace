5193b19f05eba5d3d63305cfbca5f357f81cc199
Caleb <kai_sunny@icloud.com>
2024-07-01 11:29:09 +0800

    HW3刪檔
    HW3刪檔

---
+++ CIA101036-Homework/src/HW3/TestTriangle.java
@@ -0,0 +1,16 @@
+package HW3;
+
+import java.util.Scanner;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please input three number :");
+        int[] triangleArray = sc.nextInt();
+        for (int i = 0; triangleArray.length < 3; i++ ){
+
+        }
+
+
+    }
+}
